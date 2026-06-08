79923857963dcbae9ee90864b207475fade29abe
piyush72yaduvanshi <piyushyaduvanshi6574@gmail.com>
2026-06-08 19:00:27 +0530

    feat: add CRUD operations for posts including fetching, updating, and deleting

--- src/post.controller.js
+++ src/post.controller.js
@@ -32,3 +32,119 @@ export const createPost = async (req, res) => {
     });
   }
 };
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        author: true,
+      },
+    });
+    if (!posts) {
+      return res.status(404).json({
+        message: "Posts not found",
+      });
+    }
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      posts: posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while getting posts",
+      error: error.message,
+    });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const { title, postText } = req.body;
+    const id = parseInt(req.params.id);
+    const post = await prisma.post.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title: title,
+        postText: postText,
+      },
+    });
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.status(200).json({
+      message: "Post updated successfully",
+      post: post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server error",
+      error: error.message,
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    if (!id) {
+      return res.status(400).json({
+        message: "Post id is required",
+      });
+    }
+    const post = await prisma.post.delete({
+      where: {
+        id: id,
+      },
+    });
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.status(200).json({
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server error",
+      error: error.message,
+    });
+  }
+};
+
+export const getById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    if(!id){
+      return res.status(400).json({
+        message: "Post id is required"
+      })
+    }
+    const post = await prisma.post.findUnique({
+      where: {
+        id: id
+      },
+      include: {
+        author: true
+      }
+    })
+    if(!post){
+      return res.status(404).json({
+        message: "Post not found"
+      })
+    }
+    res.status(200).json({
+      message: "Post fetched successfully",
+      post: post
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server error",
+      error: error.message
+    })
+  }
+}
--- src/post.routes.js
+++ src/post.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { isAuthenticated } from "./middleware.js";
-import { createPost } from "./post.controller.js";
+import { createPost, getPosts, deletePost, updatePost,getById } from "./post.controller.js";
 
 const router = express.Router();
 
 router.post("/create-post", isAuthenticated, createPost);
+router.get("/get-posts", isAuthenticated, getPosts);
+router.delete("/delete-post/:id", isAuthenticated, deletePost);
+router.put("/update-post/:id", isAuthenticated, updatePost);
+router.get("/get-post/:id", isAuthenticated, getById);
 
 export default router;
